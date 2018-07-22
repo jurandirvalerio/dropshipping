252fcceee0975d268fcd7d9947c6f0b9b3da1ebb
Jurandir Valerio <jurandir_valerio@hotmail.com>
2018-07-22 10:57:01 -0300

    Controllers.

---
+++ Dropshipping/WebApplication/Areas/Loja/Controllers/ClienteController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication.Areas.Loja.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Loja/Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dropshipping/WebApplication/Areas/Loja/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Areas.Loja.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Loja/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
